a50bfbd0f808e31c4823d35bfd43ad42e3cbb256
Jonathan Gil <jhongilp@gmail.com>
2024-01-20 16:10:27 -0500

    create Supabase Auth client

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+export * from "api/customer.api"
+export * from "api/expo.api"
+
+export const supabase = createClient(
+  "https://edmyrapaqwyonmwrazea.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbXlyYXBhcXd5b25td3JhemVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0NTE5OTcsImV4cCI6MjAyMTAyNzk5N30.Z6UG91gwxr_6qIG8AZmRcQB758UtJ_MB0to4vw-xCok"
+);
