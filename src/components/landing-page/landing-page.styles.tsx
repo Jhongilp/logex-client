9010aeec38bde8ec0565db3814d6866552fbc1d4
Jonathan Gil <jhongilp@gmail.com>
2024-01-21 13:40:48 -0500

    update Signup form style

--- src/components/landing-page/landing-page.styles.tsx
+++ src/components/landing-page/landing-page.styles.tsx
@@ -15,31 +15,8 @@ export const SignUpWrapper = styled.div`
 `;
 
 export const SignUpForm = styled(StyledForm)`
-  > div.user-company_name {
-    grid-area: 1 / 1 / 2 / 7;
-  }
-  > div.user-company_id {
-    grid-area: 1 / 7 / 2 / 13;
-  }
-  > div.user-first_name {
-    grid-area: 2 / 1 / 3 / 7;
-  }
-  > div.user-second_name {
-    grid-area: 2 / 7 / 3 / 13;
-  }
-  > div.user-first_lastname {
-    grid-area: 3 / 1 / 4 / 7;
-  }
-  > div.user-second_lastname {
-    grid-area: 3 / 7 / 4 / 13;
-  }
+  grid-template-columns: 1fr 1fr;
   > div.user-email {
-    grid-area: 4 / 1 / 5 / 7;
-  }
-  > div.user-password1 {
-    grid-area: 5 / 1 / 6 / 7;
-  }
-  > div.user-password2 {
-    grid-area: 5 / 7 / 6 / 13;
+    grid-column: 1/3;;
   }
 `;
