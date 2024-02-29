6ae1bba27c9889c2c24fccc2ad3066355900bbb2
Jonathan Gil <jhongilp@gmail.com>
2024-02-28 21:29:04 -0500

    add subscription hook for shipping

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,7 +1,9 @@
 import { useAppDispatch } from "hooks/store.hooks";
 import { useSubscription } from "urql";
 import { updateCustomers } from "components/customer/reducers/customerSlice";
-import { onCustomerUpdates } from "api";
+import { shippingActions } from "components/customer/reducers/shippingSlice";
+
+import { onCustomerUpdates, onShippingUpdates } from "api";
 
 export const useInitializeSubscriptions = () => {
   const dispatch = useAppDispatch();
@@ -12,6 +14,17 @@ export const useInitializeSubscriptions = () => {
         response?.customers
       );
       dispatch(updateCustomers(response?.customers));
+    }
+  });
+
+  useSubscription({ query: onShippingUpdates }, (_, response) => {
+    console.log("[shippings] subs, res: ", response);
+    if (response?.shippings) {
+      console.log(
+        "[shippings] dispatching from subscription",
+        response?.shippings
+      );
+      dispatch(shippingActions.addAll(response?.shippings));
     }
   });
 };
