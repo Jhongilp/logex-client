df8a0d52eb68396f7b97a8765a345e0668ebcc04
Jonathan Gil <jhongilp@gmail.com>
2024-06-22 17:23:07 -0500

    remove initialExpoSettingList client object

--- src/app_constants/index.tsx
+++ src/app_constants/index.tsx
@@ -1,10 +1,4 @@
-import {
-  ExpoActivityList,
-  ExpoStatus,
-  ProgressStatus,
-  IContainer,
-  SupplierRole
-} from "types";
+import { IContainer, SupplierRole } from "types";
 
 import {
   PropertyType,
@@ -87,153 +81,6 @@ export const initialSupplierTypeOptions: SelectTypeBoxOptionTagList = [
   },
 ];
 
-export const initialExpoSettingList: ExpoActivityList = [
-  {
-    id: "1",
-    name: "Solicitud de reserva",
-    status: ExpoStatus.PrevioCargue,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "2",
-    name: "Confirmación de reserva",
-    status: ExpoStatus.PrevioCargue,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "3",
-    name: "Coordinación de transporte terrestre",
-    status: ExpoStatus.PrevioCargue,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "4",
-    name: "Notificación al agente de aduanas",
-    status: ExpoStatus.PrevioCargue,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "5",
-    name: "Cargue en planta",
-    status: ExpoStatus.PrevioCargue,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "6",
-    name: "Ingreso a puerto",
-    status: ExpoStatus.TransitoPuerto,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "7",
-    name: "Aprobación documento de transporte",
-    status: ExpoStatus.EnPuerto,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "8",
-    name: "Confirmación zarpe",
-    status: ExpoStatus.EnPuerto,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "9",
-    name: "Notificar al cliente",
-    status: ExpoStatus.TransitoInternacional,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "10",
-    name: "Enviar documentos originales al cliente",
-    status: ExpoStatus.TransitoInternacional,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "11",
-    name: "Confirmación arribo a destino",
-    status: ExpoStatus.TransitoInternacional,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "12",
-    name: "Confirmación entrega final al cliente",
-    status: ExpoStatus.Finalizado,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "13",
-    name: "DEX",
-    status: ExpoStatus.Finalizado,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "14",
-    name: "Legalización divisas",
-    status: ExpoStatus.EnDestino,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "15",
-    name: "Liquidación",
-    status: ExpoStatus.EnDestino,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-  {
-    id: "16",
-    name: "Aprobación documentos y archivo",
-    status: ExpoStatus.EnDestino,
-    progress: ProgressStatus["Sin iniciar"],
-    responsible: "",
-    optional: false,
-    enabled: true,
-  },
-];
-
 export const shipmentTableColumns: IColumn<IContainer>[] = [
   {
     type: PropertyType.Text,
@@ -285,8 +132,6 @@ export const shipmentTableColumns: IColumn<IContainer>[] = [
   },
 ];
 
-
-
 export const expoTableColumns: IColumn<IContainer>[] = [
   {
     type: PropertyType.Text,
@@ -294,4 +139,4 @@ export const expoTableColumns: IColumn<IContainer>[] = [
     fieldName: "container_number",
     minWidth: 150,
   },
-]
+];
