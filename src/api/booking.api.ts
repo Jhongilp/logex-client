5e871f43fc6af22b9dc69c9a1826fbc5718fb94c
Jonathan Gil <jhongilp@gmail.com>
2024-06-26 17:34:22 -0500

    add createBookingMutation

---
+++ src/api/booking.api.ts
@@ -0,0 +1,28 @@
+import { gql } from "urql";
+
+export const createBookingMutation = gql`
+  mutation createBooking($input: CreateBookingInput) {
+    createBooking(input: $input) {
+      expoId
+      consignee
+      notify
+      shippingCompany
+      broker
+      transportMode
+      cityBondPort
+      bondPort
+      destinationCountry  
+      destinationCity
+      bookingNumber
+      billOfLandingId
+      vesselName
+      voyage
+      eta
+      etd
+      etaDestination
+      documentsDeadline
+      inPortDeadline
+      rollover
+    }
+  }
+`;
