604fe5a55104e643e5c2cc6fe3b2c63607f05177
Jonathan Gil <jhongilp@gmail.com>
2024-07-18 19:08:22 -0500

    add initial intro text to landing page

--- src/components/landing-page/Home.tsx
+++ src/components/landing-page/Home.tsx
@@ -1,20 +1,59 @@
 import styled from "styled-components";
+import dashboardImage from "images/logexDashboard.jpg";
 
 const Wrapper = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: 1fr 1fr;
+  /* grid-template-columns: 1fr minmax(200px, auto); */
+  grid-template-columns: 4fr 8fr;
+  grid-gap: 20px;
+  padding-top: 4em;
+`;
+
+// const IntroWrapper = styled.div`
+//   display: flex;
+//   gap: 20px;
+// `;
+
+const IntroWrapperText = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > h1 {
+    margin: 0;
+    font-size: 46px;
+    font-family: "Roboto";
+    font-weight: 400;
+  }
+`;
+
+const IntroImage = styled.div`
+  display: flex;
+  width: 100%;
+
+  > img {
+    width: 100%;
+    object-fit: none;
+    object-position: 0% 0%;
+  }
 `;
 
 export const Home = () => (
   <Wrapper>
-    <div>
-      <h1>CONTROLA TUS OPERACIONES DE COMERCIO EXTERIOR</h1>
+    <IntroWrapperText>
+      <h1>Controla tus operaciones de comercio exterior</h1>
       <p>
         Plataforma online que te permite coordinar fácilmente tus operaciones de
         comercio exterior.
       </p>
-    </div>
-    <div></div>
+      <p>
+        Ya no manejes más hojas de cálculo, con Logex puedes llevar toda la
+        información de tus operaciones en un solo lugar, en línea y evitando la
+        duplicidad de información.
+      </p>
+    </IntroWrapperText>
+    <IntroImage>
+      <img src={dashboardImage} alt="Home" />
+    </IntroImage>
   </Wrapper>
 );
